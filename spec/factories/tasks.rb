655cf97c6e0b2d31d2e3f03d95015274e0dfca4c
KentarouTawara <sironagasuken2@gmail.com>
2020-07-05 15:55:12 +0900

    [Fix]statusの設定値を変更

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     sequence(:title,'test_1')
     content { 'テスト用に使用される内容です' }
-    status { 'todo' }
+    status { :todo }
     deadline { 1.week.from_now }
     association :user
   end
