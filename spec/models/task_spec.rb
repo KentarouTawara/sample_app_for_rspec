24a90725ff07247a99bf4f26b7710c5bee1b0e02
KentarouTawara <sironagasuken2@gmail.com>
2020-07-05 16:12:16 +0900

    [Fix]マッチャをincludeからeqに変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -11,18 +11,18 @@ RSpec.describe Task, type: :model do
     it 'タイトルがない場合、無効である' do
       task_without_title  = build(:task, title: nil)
       expect(task_without_title).to be_invalid
-      expect(task_without_title.errors[:title]).to include("can't be blank")
+      expect(task_without_title.errors[:title]).to eq ["can't be blank"]
     end
     it 'タイトルが一意でない場合、無効である' do
       task1 = create(:task)
       task2 = build(:task, title: task1.title)
       expect(task2).to be_invalid
-      expect(task2.errors[:title]).to include('has already been taken')
+      expect(task2.errors[:title]).to eq ['has already been taken']
     end
     it 'ステータスがない場合、無効である' do
       task_without_status = build(:task, status: nil)
       expect(task_without_status).to be_invalid
-      expect(task_without_status.errors[:status]).to include("can't be blank")
+      expect(task_without_status.errors[:status]).to eq ["can't be blank"]
     end
   end
 end
