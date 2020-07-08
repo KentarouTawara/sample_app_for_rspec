b3629db5399a79e54de8368824a695b5c8deaf40
KentarouTawara <sironagasuken2@gmail.com>
2020-07-08 16:57:53 +0900

    UserSessionsのシステムスペックの作成

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,9 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
-  before do
-    driven_by(:rack_test)
+  describe 'ログイン前' do
+    before do
+      @user = create(:user)
+    end
+    context 'フォームの入力値が正常' do
+      it 'ログイン処理が成功する' do
+        visit login_path
+        fill_in 'Email', with: @user.email
+        fill_in 'Password', with: 'password'
+        click_button 'Login'
+        expect(current_path).to eq root_path
+        expect(page).to have_content 'Login successful'
+      end
+    end
+    context 'フォームが未入力' do
+      it 'ログイン処理が失敗する' do
+        visit login_path
+        fill_in 'Email', with: ''
+        fill_in 'Password', with: ''
+        click_button 'Login'
+        expect(current_path).to eq login_path
+        expect(page).to have_content 'Login failed'
+      end
+    end
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ログイン後' do
+    before do
+      @user = create(:user)
+      visit login_path
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: 'password'
+      click_button 'Login'
+    end
+    context 'ログアウトボタンをクリック' do
+      fit 'ログアウト処理が成功する' do
+        click_link 'Logout'
+        expect(current_path).to eq root_path
+      end
+    end
+  end
 end
